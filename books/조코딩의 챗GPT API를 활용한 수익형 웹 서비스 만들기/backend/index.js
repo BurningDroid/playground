06eabf09657f3cae1777153f637ffce6ce05e4ab
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2024-10-27 18:01:45 +0900

    backend json 응답

--- books/조코딩의 챗GPT API를 활용한 수익형 웹 서비스 만들기/backend/index.js
+++ books/조코딩의 챗GPT API를 활용한 수익형 웹 서비스 만들기/backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const OpenAI = require('openai');
 const client = new OpenAI({
-  apiKey: process.env.API_KEY_OPENAI
+    apiKey: process.env.API_KEY_OPENAI
 });
 
 const express = require('express');
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/fortuneTell', async function(req, res) {
+app.post('/fortuneTell', async function (req, res) {
     const completion = await client.chat.completions.create({
         messages: [
             { role: 'system', content: '당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다.' },
@@ -25,10 +25,10 @@ app.post('/fortuneTell', async function(req, res) {
         max_tokens: 100,
         temperature: 0.5,
     });
-    
+
     let fortune = completion.choices[0].message['content'];
     console.log(fortune);
-    res.send(fortune);
+    res.json({ "assistant": fortune });
 })
 
 app.listen(3000);
