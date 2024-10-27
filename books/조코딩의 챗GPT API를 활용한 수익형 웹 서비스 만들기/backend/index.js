5a55dd91587fb7cbe6544b031da33225f7ea50df
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2024-10-27 17:44:18 +0900

    CORS 연동

--- books/조코딩의 챗GPT API를 활용한 수익형 웹 서비스 만들기/backend/index.js
+++ books/조코딩의 챗GPT API를 활용한 수익형 웹 서비스 만들기/backend/index.js
@@ -6,8 +6,10 @@ const client = new OpenAI({
 });
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
