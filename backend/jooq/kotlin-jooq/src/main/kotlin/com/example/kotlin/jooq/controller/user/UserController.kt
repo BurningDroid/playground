c44200cdad07d7faf9b6676cf18d297cd4148887
Dev.Aaron <youknow.yoonho@gmail.com>
2025-04-17 22:57:54 +0900

    Update UserController.kt

--- backend/jooq/kotlin-jooq/src/main/kotlin/com/example/kotlin/jooq/controller/user/UserController.kt
+++ backend/jooq/kotlin-jooq/src/main/kotlin/com/example/kotlin/jooq/controller/user/UserController.kt
@@ -1,8 +1,12 @@
 package com.example.kotlin.jooq.controller.user
 
+import com.example.kotlin.jooq.api.ApiResponse
+import com.example.kotlin.jooq.dto.user.UserCreateServiceDto
 import com.example.kotlin.jooq.dto.user.UserRequest
 import com.example.kotlin.jooq.services.UserService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,14 +19,16 @@ class UserController(
 ) {
 
     @PostMapping("/v1")
-    fun create1(@Valid @RequestBody request: UserRequest): Long {
-        val user = request.toCreateServiceDto()
-        return userService.create1(user)
+    fun create1(@Valid @RequestBody request: UserRequest): ResponseEntity<ApiResponse<Long>> {
+        val serviceDto: UserCreateServiceDto = request.toCreateServiceDto()
+        val data: Long = userService.create1(serviceDto)
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success(data))
     }
 
     @PostMapping("/v2")
     fun create2(@Valid @RequestBody request: UserRequest): Long {
-        val user = request.toCreateServiceDto()
-        return userService.create2(user)
+        val serviceDto = request.toCreateServiceDto()
+        return userService.create2(serviceDto)
     }
 }
