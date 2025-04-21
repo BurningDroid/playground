aa1a0a9a24e1eebffa41779da6d15447a0494cf5
Dev.Aaron <youknow.yoonho@gmail.com>
2025-04-21 23:52:30 +0900

    Dao 실습: FilmRepositoryHasA

--- backend/jooq/jooqfirstlook/src/test/kotlin/com/aaron/jooqfirstlook/JooqDaoWrapperTest.kt
+++ backend/jooq/jooqfirstlook/src/test/kotlin/com/aaron/jooqfirstlook/JooqDaoWrapperTest.kt
@@ -16,8 +16,24 @@ class JooqDaoWrapperTest {
 	lateinit var filmRepoHasA: FilmRepositoryHasA
 
 	@Test
-	fun test() {
-		val film = filmRepoIsA.findById(1)
-		assertThat(film).isNotNull
+	fun isATest() {
+		val start = 100
+		val end = 180
+
+		val films = filmRepoIsA.fetchRangeOfJLength(start, end)
+		assertThat(films).allSatisfy { film ->
+			assertThat(film.length).isBetween(start, end)
+		}
+	}
+
+	@Test
+	fun hasATest() {
+		val start = 100
+		val end = 180
+
+		val films = filmRepoHasA.findByRangeBetween(start, end)
+		assertThat(films).allSatisfy { film ->
+			assertThat(film.length).isBetween(start, end)
+		}
 	}
 }
